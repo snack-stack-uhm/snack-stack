1831a1021f32b42bf1bb242fd059e61a6d58778c
Justin <justin@practicalsoftware.com>
2026-01-28 10:23:59 -1000

    Update page.tsx

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -180,7 +180,9 @@ export default function SignInPage() {
       {!showVerification && (
         <div className={styles.formWrapper}>
           <h1 className={styles.title}>Sign In</h1>
-          <p className={styles.descriptionCentered}>Enter your email and password to access your Snack Stack account.</p>
+          <p className={styles.descriptionCentered}>
+          Enter your email and password to access your Snack Stack account.
+          </p>
           <form onSubmit={handleSubmit}>
             <div className={styles.inputGroup}>
               <label className={styles.label}>Email</label>
